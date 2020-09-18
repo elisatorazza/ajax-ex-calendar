ee20efa2feda41824f0036a8b58b3d21a52b2e2d
Elisa <elisa.torazza@gmail.com>
2020-09-18 17:34:44 +0200

    Chiamata ajax

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 $(document).ready(function() {
 
+  var date = moment("01-01-2018", "DD-MM-YYYY");
 
-  for (var i = 1; i<=31; i++) {
+  for (var i = 1; i<=date.daysInMonth(); i++) {
 
     var source = $("#entry-template").html();
     var template = Handlebars.compile(source);
 
     var startDate = {
       "day" : i,
-      "month" : "January",
+      "month" : date.format("MMMM"),
       "year" : 2018,
     }
     var html = template(startDate);
+    
     $(".date-list").append(html);
+    $.ajax (
+      {
+       "url": "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+       "method": "GET",
+       "success": function (data, stato){
+        if (data["response"].date == startDate) {
+          $(".day").addClass("holiday");
+        }
+       }
+    }
+    )
   }
+
+
 
 })
