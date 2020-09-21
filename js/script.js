8f01aa99c1d3dc4450d91036cf9dab2249f41793
Elisa <elisa.torazza@gmail.com>
2020-09-21 15:56:08 +0200

    Iniziato  a creare condizioni

--- js/script.js
+++ js/script.js
@@ -26,30 +26,33 @@ $(".previous").click(function (){
 $(".next").click(function (){
  if (month == 11) {
    alert("Impossibile verificare calendario successivo");
- } else //if (date.daysInMonth() == 31){
-   {var newDate = moment("01-01-2018", "DD-MM-YYYY");
-   console.log(newDate);
-   var newDate = moment(newDate).add(31, 'days');
+ } else if (date.daysInMonth() == 31){
+   var newDate = moment(date).add(31, 'days');
+    $(".date-list").empty();
+   startMonth = printCalendar(newDate);
+   startHoliday = printHoliday(newDate);
+   date = newDate;
    console.log(newDate);
    console.log(newDate.format('YYYY-MM-DD'));
-   // var month = newDate.month();
-   // printCalendar (newDate);
-   // printHoliday (newDate);
  } //else if (date.daysInMonth() == 30) {
  //   var newDate = moment(newDate).add(30, 'days');
+ //   startMonth = printCalendar(newDate);
+ //   startHoliday = printHoliday(newDate);
+ //   date = newDate;
  // } else if (date.daysInMonth() == 28){
  //   var newDate = moment(newDate).add(28, 'days');
- // }
- startMonth = printCalendar(newDate);
- startHoliday = printHoliday(newDate);
-})
+ //   startMonth = printCalendar(newDate);
+ //   startHoliday = printHoliday(newDate);
+ //   date = newDate;
+ //}
 
+})
 
 // funzione per stampare il calendario//
 function printCalendar (date) {
   var daysInMonth = date.daysInMonth();
 
-  $("h1").text(moment().month(month).format("MMMM"));
+  $('h1').html(date.format('MMMM YYYY'));
 
   for (var i = 1; i<=daysInMonth; i++) {
 
@@ -64,7 +67,6 @@ function printCalendar (date) {
       }
 
       var html = template(startDate);
-
       $(".date-list").append(html);
       date.add(1, "day");
       }
